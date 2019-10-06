a5bd3bce2c921f5c369670ff1cb03c7f9d401ef5
nitin khanna <khannanitin106@gmail.com>
2019-10-06 07:05:46 -0700

    second commit

---
+++ src/main/java/com/example/khanna/NitinApplication.java
@@ -0,0 +1,13 @@
+package com.example.khanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/khanna/NitinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.khanna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NitinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
